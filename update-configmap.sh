ebf46c04d75295c0217f8b525754b2e61397539d
ticiano <ticiano@tre-ce.jus.br>
2022-12-01 10:51:41 -0300

    Script to update configmap

---
+++ update-configmap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash    
+    
+# Git pull    
+echo -ne "Updating local yamls..."     
+git pull -v    
+echo "done"     
+    
+# Deletes old configmap    
+echo -ne "Removing old configmap... "     
+kubectl delete configmap -n monitoring prometheus-config    
+echo "done"     
+    
+echo -ne "Creating new configmap..."       
+# Creates configmap    
+kubectl create configmap prometheus-config --from-file=prometheus=prometheus.yml --from-file=prometheus-alerts=alerts.yml -n monitoring    
+echo "done"    
